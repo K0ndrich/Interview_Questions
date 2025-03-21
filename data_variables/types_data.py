b45d804b06316be7371ba2717abe5db3ac3355b3
K0ndrich <123kondrich@gmail.com>
2025-03-21 15:09:35 +0200

    asdasd

---
+++ data_variables/types_data.py
@@ -0,0 +1,35 @@
+# Какие есть Типы Данных в Python ?
+
+
+# ---   ИЗМЕНЯЕМЫЕ   -->
+
+# - Списки (list) my_list = [3, 2.7 , True, "hello"]
+
+# - Словари (dict) my_dict = {'a': 1, 'b': 5.3}
+
+# - Множества (set) my_set = (1, 2, 3)
+
+# - Масивы Байтов (bytearray) my_bytearray = (b"hello")
+
+# - Пользовательские Классы
+# class MyClassName:
+#     def __init__(self, name):
+#         self.name = name
+
+# p = Person("Alice")
+# p.name = "Bob"
+
+
+# -- НЕ ИЗМЕНЯЕМЫЕ ->
+
+# - Целые Числа (int) a = 3
+
+# - Числа с Плавающей Точкой / Дробные (float) a = 3.7
+
+# - Комплексные Числа (complex) a = 3 + 4j
+
+# - Кортеж (tuple) my_tuple = (1,2,3)
+
+# - Неизменяемые Множества (frozensets) = my_frozenset=([1,2,3])
+
+# - Байтовые Строки (bytes) my_bytes = b'hello'
